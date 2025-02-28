c910f567ea623f282984ca9be6211bd6cfe12a11
IsaqueCN <ggomesnascimento123@gmail.com>
2025-02-28 15:35:06 -0300

    Adicionar arquivos de projeto.

---
+++ Website ASPNet/Program.cs
@@ -0,0 +1,15 @@
+namespace Website_ASPNet
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}

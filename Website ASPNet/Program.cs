6583c19bf021577270aa68adb50eb79833f3469e
IsaqueCN <ggomesnascimento123@gmail.com>
2025-02-28 16:29:45 -0300

    Adding middlewares for 404 responses and to ensure utf-8 charset

--- Website ASPNet/Program.cs
+++ Website ASPNet/Program.cs
@@ -7,7 +7,34 @@ namespace Website_ASPNet
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.Use(async(context, next) =>
+            {
+                context.Response.OnStarting(() =>
+                {
+                    if (context.Response.ContentType != null && !context.Response.ContentType.Contains("charset"))
+                    {
+                        context.Response.ContentType += "; charset=utf-8";
+                    }
+                    return Task.CompletedTask;
+                });
+
+                await next();
+            });
+
+            app.Use(async (context, next) =>
+            {
+                await next();
+
+                if (context.Response.StatusCode == 404)
+                {
+                    context.Response.ContentType = "text/html";
+                    await context.Response.WriteAsync("<h1>Page not found!</h1>");
+                }
+            });
+
+            app.MapGet("/", (HttpContext context) => {
+                return Results.Content("<h1>You are in the main page!", "text/html");
+            });
 
             app.Run();
         }
